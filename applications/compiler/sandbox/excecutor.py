46610b7b6a879a4eff3353b981c1b56c7d8b3b9d
CallemeAK09 <mr.ak11718@gmail.com>
2026-08-07 19:13:25 +0530

    refactor: added excution utility.

---
+++ applications/compiler/sandbox/excecutor.py
@@ -0,0 +1,25 @@
+import contextlib
+
+from django.db import models 
+
+@contextlib.contextmanager
+def enforce_validation():
+    """
+    Monkey-patch models.Model.save to call full_clean() before saving.
+    This ensures that Django's validation (max_length, blank, etc.) is enforced.
+    """
+
+    original_save = models.Model.saved
+
+    def save_validated(self, *args, **kwargs):
+        # We call full_clean() to trigger Django's field validation.
+        # This will raise a ValidationError if constraints are violated.
+        self.full_clean()
+        return original_save(self, *args, **kwargs)
+
+    models.Model.save = save_validated
+
+    try:
+        yield
+    finally:
+        models.model.save = original_save
